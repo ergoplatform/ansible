45e18c2b8116567ebb7509ab9c264d52d3212ec5
andyceo <andyceo@yandex.ru>
2018-04-16 14:59:55 +0300

    Update Jenkins OpenAPI validity script

--- files/scripts/check_openapi_spec.sh
+++ files/scripts/check_openapi_spec.sh
@@ -2,4 +2,14 @@
 
 pip3 install ansicolors openapi-spec-validator
 wget -N https://raw.githubusercontent.com/ergoplatform/ansible/master/files/scripts/validator.py
+npm i speccy
+
+# Python OpenAPI validator
+echo
+echo "Validating with Python openapi-spec-validator..."
 python3 validator.py src/main/resources/api/openapi.yaml
+
+# NodeJS OpenAPI validator
+echo
+echo "Validating with NodeJS speccy..."
+node_modules/.bin/speccy lint -s openapi-tags src/main/resources/api/openapi.yaml
