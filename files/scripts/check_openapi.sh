b72bbcb19ae2f58090c3fe883a1c6620bb9b7f1d
andyceo <andyceo@yandex.ru>
2018-04-18 18:25:12 +0300

    Improve check_openapi.sh script - now it can validate responses

--- files/scripts/check_openapi.sh
+++ files/scripts/check_openapi.sh
@@ -1,15 +1,34 @@
 #!/usr/bin/env sh
 
-pip3 install ansicolors openapi-spec-validator
+set -e
+
+SPEC_IS_WRONG=false
+
+pip3 install ansicolors openapi-core openapi-spec-validator
 wget -N https://raw.githubusercontent.com/ergoplatform/ansible/master/files/scripts/check_openapi_spec.py
+wget -N https://raw.githubusercontent.com/ergoplatform/ansible/master/files/scripts/check_openapi_responses.py
 npm i speccy
 
 # Python OpenAPI validator
 echo
 echo "Validating with Python openapi-spec-validator..."
 python3 check_openapi_spec.py src/main/resources/api/openapi.yaml
+if [ $? -ne 0 ]; then
+    SPEC_IS_WRONG=true
+fi
 
 # NodeJS OpenAPI validator
 echo
 echo "Validating with NodeJS speccy..."
 node_modules/.bin/speccy lint -s openapi-tags src/main/resources/api/openapi.yaml
+if [ $? -ne 0 ]; then
+    SPEC_IS_WRONG=true
+fi
+
+# Exiting if OpenAPI specification did not pass validation
+if ${SPEC_IS_WRONG}; then
+    echo "OpenAPI specification is wrong, exiting..."
+    exit 1
+fi
+
+python3 check_openapi_responses.py src/main/resources/api/openapi.yaml
