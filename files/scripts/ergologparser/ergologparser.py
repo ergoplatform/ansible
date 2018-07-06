ab68f47f76c8060357637ae27d1606d5289a3efa
andyceo <andyceo@yandex.ru>
2018-07-06 12:52:57 +0300

    ergologparser now can run only by Python interpreter only

--- files/scripts/ergologparser/ergologparser.py
+++ files/scripts/ergologparser/ergologparser.py
@@ -2,20 +2,21 @@
 
 import re
 
-path = '/data/ergo'
-log_file = path + '/l.log'
-config_file = path + '/application.conf'
+if __name__ == "__main__":
+    path = '/data/ergo'
+    log_file = path + '/l.log'
+    config_file = path + '/application.conf'
 
-with open(config_file, 'r') as c:
-    config = c.read()
-    match = re.search('nodeName = "(.+)"', config)
-    if match:
-        node_name = match.group(1)
-        with open(log_file, 'r') as l:
-            for logstring in l:
-                if 'New block found:' in logstring:
-                    match = re.search('"id":"(.+?)"', logstring)
-                    if match:
-                        block_id = match.group(1)
-                        print(node_name, '|', block_id)
-                        break
+    with open(config_file, 'r') as c:
+        config = c.read()
+        match = re.search('nodeName = "(.+)"', config)
+        if match:
+            node_name = match.group(1)
+            with open(log_file, 'r') as l:
+                for logstring in l:
+                    if 'New block found:' in logstring:
+                        match = re.search('"id":"(.+?)"', logstring)
+                        if match:
+                            block_id = match.group(1)
+                            print(node_name, '|', block_id)
+                            break
