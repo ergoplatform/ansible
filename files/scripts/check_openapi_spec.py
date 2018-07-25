c860090e2e0aaed9bd1d66366e62ced6c708ee22
andyceo <andyceo@yandex.ru>
2018-07-25 17:45:58 +0300

    Prettify check_openapi_spec.py script

--- files/scripts/check_openapi_spec.py
+++ files/scripts/check_openapi_spec.py
@@ -21,59 +21,35 @@ def validate(url):
 
         for i in openapi_v3_spec_validator.iter_errors(spec, spec_url=url):
             counter += 1
-            print_error(
-                counter,
-                ':'.join(i.absolute_path),
-                i.message,
-                i.instance
-            )
+            print_error(counter, ':'.join(i.absolute_path), i.message, i.instance)
 
     except RefResolutionError as e:
         counter += 1
-        print_error(
-            counter,
-            '',
-            'Unable to resolve {} in {}'.format(e.__context__.args[0], e.args[0]),
-            ''
-        )
+        print_error(counter, '', 'Unable to resolve {} in {}'.format(e.__context__.args[0], e.args[0]), '')
+
     except BaseException:
         counter += 1
         print_error(counter, '', sys.exc_info()[0], '')
+
     finally:
         if counter > 0:
             print()
-            print(
-                color(
-                    ' [FAIL] %d errors found ' % counter,
-                    fg='white',
-                    bg='red',
-                    style='bold'
-                )
-            )
+            print(color(' [FAIL] {:d} errors found '.format(counter), fg='white', bg='red', style='bold'))
             return 1
         else:
-            print(
-                color(
-                    ' [PASS] No errors found ',
-                    fg='white',
-                    bg='green',
-                    style='bold'
-                )
-            )
+            print(color(' [PASS] No errors found ', fg='white', bg='green', style='bold'))
             return 0
 
 
 def print_error(count, path, message, instance):
     print()
-    print(
-        color('Error #%d in [%s]:' % (count, path or 'unknown'), style='bold')
-    )
-    print("    %s" % message)
-    print("    %s" % instance)
+    print(color('Error #{:d} in [{}]:'.format(count, path or 'unknown'), style='bold'))
+    print("    {}".format(message))
+    print("    {}".format(instance))
 
 
 def help():
-    print('usage: ' + path.basename(__file__) + ' <spec>')
+    print('usage: ' + path.basename(__file__) + ' <spec_url_or_path>')
 
 
 def main(argv):
