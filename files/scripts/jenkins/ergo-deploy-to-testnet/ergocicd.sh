668216501678177f4dffe0290c3e2d5a13bdd21f
andyceo <andyceo@yandex.ru>
2018-08-03 17:47:16 +0300

    Bugfix - now ergocicd will kill every ergo process before executing new one

--- files/scripts/jenkins/ergo-deploy-to-testnet/ergocicd.sh
+++ files/scripts/jenkins/ergo-deploy-to-testnet/ergocicd.sh
@@ -7,7 +7,7 @@ NODE_CONFIG=${NODE_BASE_DIR}/application.conf
 NODE_LOG=${NODE_BASE_DIR}/ergo_norotate.log
 NODE_PARAMS=
 NODE_DATA_DIR=${NODE_BASE_DIR}/data
-PID=$(pgrep -f "ergo*.jar")
+PIDS=$(pgrep -f "ergo.*\.jar")
 FOUND=$?
 WIPEDATA=${WIPEDATA:-false}
 VERSION=${VERSION:-master}
@@ -17,32 +17,33 @@ JARFILE_TO_SEEK="ergo-deploy-to-testnet-${VERSION}.jar"
 # Variables BUILD_ID, BUILD_TAG, BUILD_URL and others are provided by Jenkins
 
 # This script does not work with `set -xe`
-set -x
 
-if echo ${PID} | grep -q ' '; then
-
-    echo "Error: More than one process found. Quitting." >&2
-    exit 1
-
-elif [ "${FOUND}" -eq 0 ] && [ -n "${PID}" ]; then
-
-    if ! kill ${PID}; then
-        echo "Error: Am not allowed to kill process ${PID}. Quitting." >&2
-        exit 2
-    fi
-
-    echo "Killing process ${PID} and wait ${MAXWAIT} seconds until it die"
-    i=0
-    while kill -0 ${PID}; do
-        if [ "$i" -gt "${MAXWAIT}" ]; then
-            echo "Error: Waited more than ${MAXWAIT} seconds for process with pid ${PID} to die. Giving up." >&2
-            exit 3
-        fi
-        i=$((i + 1))
-        sleep 1
-    done
-
+if echo ${PIDS} | grep -q ' '; then
+    echo "Error: More than one process found. Killing processes ${PIDS} subsequentially, one by one." >&2
 fi
+
+for pid in ${PIDS}
+do
+    if [ "${FOUND}" -eq 0 ] && [ -n "${pid}" ]; then
+
+        if ! kill ${pid}; then
+            echo "Error: Am not allowed to kill process ${pid}. Quitting." >&2
+            exit 2
+        fi
+
+        echo "Killing process ${pid} and wait ${MAXWAIT} seconds until it die"
+        i=0
+        while kill -0 ${pid}; do
+            if [ "$i" -gt "${MAXWAIT}" ]; then
+                echo "Error: Waited more than ${MAXWAIT} seconds for process with pid ${pid} to die. Giving up." >&2
+                exit 3
+            fi
+            i=$((i + 1))
+            sleep 1
+        done
+
+    fi
+done
 
 if ${WIPEDATA}; then
     echo "Wiping node data..."
