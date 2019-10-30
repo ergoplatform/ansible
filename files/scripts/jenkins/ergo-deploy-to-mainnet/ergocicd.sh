238dae15e09a03eb71d963693acb7e572bb83c02
andyceo <andyceo@yandex.ru>
2019-10-30 19:20:47 +0300

    Make Ergo node eployment string universal for mainnet, testnet

--- files/scripts/jenkins/ergo-deploy-to-mainnet/ergocicd.sh
+++ files/scripts/jenkins/ergo-deploy-to-mainnet/ergocicd.sh
@@ -5,10 +5,6 @@ WASKILL9=false
 NODE_BASE_DIR=/data/ergo
 NODE_JAR=${NODE_BASE_DIR}/ergo.jar
 NODE_CONFIG=${NODE_BASE_DIR}/application.conf
-NODE_LOG=${NODE_BASE_DIR}/ergo_norotate.log
-NODE_LOG_PRESERVE_FILENAME_BASE=ergo_norotate_build_id
-NODE_LOG_PRESERVE=${NODE_BASE_DIR}/${NODE_LOG_PRESERVE_FILENAME_BASE}${BUILD_ID}.log
-MAXLOGS=10
 NODE_PARAMS="-XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/tmp/java_build_id${BUILD_ID}.hprof"
 NODE_DATA_DIR=${NODE_BASE_DIR}/data
 NODE_MEMORY_XMX=$(awk '/MemTotal/ { printf "%.0f", $2*0.9 }' /proc/meminfo)
@@ -19,11 +15,11 @@ VERSION=${VERSION:-master}
 JARFILE_TO_SEEK="ergo-deploy-to-testnet-${VERSION}.jar"
 
 # Variable INFLUXDB_CONNECTION_STRING should exists for curling to InfluxDB monitoring database
-# Variables BUILD_ID, BUILD_TAG, BUILD_URL and others are provided by Jenkins
+# Variables JOB_NAME, BUILD_ID, BUILD_TAG, BUILD_URL and others are provided by Jenkins
 
 # This script does not work with `set -xe`
 
-if echo ${PIDS} | grep -q ' '; then
+if echo "${PIDS}" | grep -q ' '; then
     echo "Error: More than one process found. Killing processes ${PIDS} subsequentially, one by one." >&2
 fi
 
@@ -55,17 +51,7 @@ do
             sleep 1
         done
 
-        echo "Process ${pid} was killed, moving previous logs from ${NODE_LOG} to ${NODE_LOG_PRESERVE}"
-        mv ${NODE_LOG} ${NODE_LOG_PRESERVE}
-
-        echo "Clean up old preserved logs..."
-        i=0
-        ls -1r ${NODE_BASE_DIR}/${NODE_LOG_PRESERVE_FILENAME_BASE}*.log | while read filename; do
-            i=$((i + 1))
-            if [ "$i" -gt "${MAXLOGS}" ]; then
-                rm filename
-            fi
-        done
+        echo "Process ${pid} was killed"
 
     fi
 done
@@ -78,11 +64,15 @@ fi
 
 echo "Starting Ergo node..."
 
-cp -pf $(find . -type f -name ${JARFILE_TO_SEEK}) ${NODE_JAR}
+cp -pf "$(find . -type f -name ${JARFILE_TO_SEEK})" ${NODE_JAR}
 
 NODE_PARAMS="-Xmx${NODE_MEMORY_XMX}K ${NODE_PARAMS}"
-echo "scorex.network.maxConnections = 100\nscorex.network.nodeName = \"$(hostname)\"" > ${NODE_CONFIG}
-BUILD_ID=dontKillMe daemon --chdir=${NODE_BASE_DIR} -- java ${NODE_PARAMS} -jar ${NODE_JAR} --mainnet -c ${NODE_CONFIG}
+if [ "${JOB_NAME}" = 'ergo-deploy-to-mainnet' ]; then
+    echo "scorex.network.maxConnections = 100\nscorex.network.nodeName = \"$(hostname)\"" > ${NODE_CONFIG}
+    BUILD_ID=dontKillMe daemon --chdir=${NODE_BASE_DIR} -- java ${NODE_PARAMS} -jar ${NODE_JAR} --mainnet -c ${NODE_CONFIG}
+else
+    BUILD_ID=dontKillMe daemon --chdir=${NODE_BASE_DIR} -- java ${NODE_PARAMS} -jar ${NODE_JAR} --testnet -c ${NODE_CONFIG}
+fi
 
 # Supress passwords output used below
 set +xe
@@ -92,9 +82,9 @@ if [ -f /etc/profile.d/testnet_env_vars.sh ]; then
     . /etc/profile.d/testnet_env_vars.sh
 
     # If this variable was read, then report about event to InfluxDB
-    if [ -n ${INFLUXDB_CONNECTION_STRING} ]; then
+    if [ -n "${INFLUXDB_CONNECTION_STRING}" ]; then
         INFLUXDB_EVENT_TITLE="Deployed ${VERSION} on $(hostname)"
         INFLUXDB_EVENT_DESCRIPTION="<a href='${BUILD_URL}'>Build info</a>"
-        curl -s -X POST ""${INFLUXDB_CONNECTION_STRING}"" --data-binary 'events title="'"${INFLUXDB_EVENT_TITLE}"'",description="'"${INFLUXDB_EVENT_DESCRIPTION}"'"'
+        curl -s -X POST ${INFLUXDB_CONNECTION_STRING} --data-binary 'events title="'"${INFLUXDB_EVENT_TITLE}"'",description="'"${INFLUXDB_EVENT_DESCRIPTION}"'"'
     fi
 fi
